16045d6f1ad4d078dfbaad62dd53badf7e8ff8b3
尾崎しょうご <syougo1999most@yahoo.co.jp>
2021-05-03 23:57:22 +0900

    backendとfrontendがつながった

--- frontend/ekdn-app/src/App.js
+++ frontend/ekdn-app/src/App.js
@@ -1,12 +1,23 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <p>aa"a"</p>
-    </div>
-  );
+const App=()=>{
+  const [persons, setPersons] = useState([])
+  useEffect( ()=>{
+    const f= async () =>{
+      const response = await axios.get("http://localhost:3000/posts");
+      setPersons(response.data)
+      console.log(response.data)
+    }
+    f();
+  },[])
+  return(
+    <>
+      {persons.map(person =>(
+        <p>{person.id}</p>
+      ))}
+    </>
+  )
 }
 
 export default App;
