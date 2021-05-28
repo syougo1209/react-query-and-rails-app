d70b635e7e66a86c5f399e3e9862394c7b465039
尾崎しょうご <syougo1999most@yahoo.co.jp>
2021-05-28 12:55:04 +0900

    ログイン機能のリファクタ

--- backend/app/controllers/concerns/login_actions.rb
+++ backend/app/controllers/concerns/login_actions.rb
@@ -12,13 +12,5 @@ module LoginActions
   def current_user
     @current_user ||= User.find_by(session[:user_id])
   end
-
-  def restrict_to_logged_in_user
-    return render_401_error if session[:user_id]
-  end
-
-  def render_401_error
-    render json: { message: "unauthorized" }, status: 401
-  end
 end
 
